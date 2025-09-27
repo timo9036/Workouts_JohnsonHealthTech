85b6e07052a2e868b9e8aac0a0ba35c9cfcf9dd2
timo9036 <timo9036@hotmail.com>
2025-09-27 02:40:16 -0500

    add repo

--- app/src/main/java/com/example/workouts_johnsonhealthtech/data/repository/WorkoutRepository.kt
+++ app/src/main/java/com/example/workouts_johnsonhealthtech/data/repository/WorkoutRepository.kt
@@ -1,4 +1,10 @@
 package com.example.workouts_johnsonhealthtech.data.repository
 
-class WorkoutRepository {
+import com.example.workouts_johnsonhealthtech.data.model.Workout
+import kotlinx.coroutines.flow.Flow
+
+interface WorkoutRepository {
+    fun getWorkouts(): Flow<List<Workout>>
+    fun getWorkoutById(id: String): Flow<Workout>
+    suspend fun updateWorkout(workout: Workout)
 }
--- app/src/main/java/com/example/workouts_johnsonhealthtech/data/repository/WorkoutRepositoryImpl.kt
+++ app/src/main/java/com/example/workouts_johnsonhealthtech/data/repository/WorkoutRepositoryImpl.kt
@@ -1,4 +1,20 @@
 package com.example.workouts_johnsonhealthtech.data.repository
 
-class WorkoutRepositoryImpl {
+import com.example.workouts_johnsonhealthtech.data.local.WorkoutDao
+import com.example.workouts_johnsonhealthtech.data.model.Workout
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class WorkoutRepositoryImpl @Inject constructor(
+    private val workoutDao: WorkoutDao
+) : WorkoutRepository {
+    override fun getWorkouts(): Flow<List<Workout>> = workoutDao.getAllWorkouts()
+
+    override fun getWorkoutById(id: String): Flow<Workout> = workoutDao.getWorkoutById(id)
+
+    override suspend fun updateWorkout(workout: Workout) {
+        workoutDao.updateWorkout(workout)
+    }
 }
