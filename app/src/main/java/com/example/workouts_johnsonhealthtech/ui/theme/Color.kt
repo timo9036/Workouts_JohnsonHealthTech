b25285894276b73ce56b77edd43c4a30bc066a5b
timo9036 <timo9036@hotmail.com>
2025-09-28 23:35:38 -0500

    added colors

--- app/src/main/java/com/example/workouts_johnsonhealthtech/ui/theme/Color.kt
+++ app/src/main/java/com/example/workouts_johnsonhealthtech/ui/theme/Color.kt
@@ -2,10 +2,68 @@ package com.example.workouts_johnsonhealthtech.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val Primary = Color(0xFF4285F4)
+val OnPrimary = Color(0xFFFFFFFF)
+val PrimaryContainer = Color(0xFFDDEBFF)
+val OnPrimaryContainer = Color(0xFF001D35)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Secondary = Color(0xFF526070)
+val OnSecondary = Color(0xFFFFFFFF)
+val SecondaryContainer = Color(0xFFD5E4F7)
+val OnSecondaryContainer = Color(0xFF0E1D2A)
+
+val Tertiary = Color(0xFF6B5778)
+val OnTertiary = Color(0xFFFFFFFF)
+val TertiaryContainer = Color(0xFFF2DAFF)
+val OnTertiaryContainer = Color(0xFF251431)
+
+val Error = Color(0xFFBA1A1A)
+val OnError = Color(0xFFFFFFFF)
+val ErrorContainer = Color(0xFFFFDAD6)
+val OnErrorContainer = Color(0xFF410002)
+
+val Background = Color(0xFFF8F9FA)
+val OnBackground = Color(0xFF1A1C1E)
+
+val Surface = Color(0xFFFDFBFF)
+val OnSurface = Color(0xFF1A1C1E)
+val SurfaceVariant = Color(0xFFDFE2EB)
+val OnSurfaceVariant = Color(0xFF42474E)
+
+val Outline = Color(0xFF73777F)
+
+// Dark Theme Colors
+val DarkPrimary = Color(0xFFADC6FF)
+val DarkOnPrimary = Color(0xFF00325A)
+val DarkPrimaryContainer = Color(0xFF274472)
+val DarkOnPrimaryContainer = Color(0xFFDDEBFF)
+
+val DarkSecondary = Color(0xFFB9C8DA)
+val DarkOnSecondary = Color(0xFF243240)
+val DarkSecondaryContainer = Color(0xFF3A4857)
+val DarkOnSecondaryContainer = Color(0xFFD5E4F7)
+
+val DarkTertiary = Color(0xFFD6BEE4)
+val DarkOnTertiary = Color(0xFF3B2947)
+val DarkTertiaryContainer = Color(0xFF523F5F)
+val DarkOnTertiaryContainer = Color(0xFFF2DAFF)
+
+val DarkError = Color(0xFFFFB4AB)
+val DarkOnError = Color(0xFF690005)
+val DarkErrorContainer = Color(0xFF93000A)
+val DarkOnErrorContainer = Color(0xFFFFDAD6)
+
+val DarkBackground = Color(0xFF121212)
+val DarkOnBackground = Color(0xFFE2E2E6)
+
+val DarkSurface = Color(0xFF1A1C1E)
+val DarkOnSurface = Color(0xFFE2E2E6)
+val DarkSurfaceVariant = Color(0xFF42474E)
+val DarkOnSurfaceVariant = Color(0xFFC3C7CF)
+
+val DarkOutline = Color(0xFF8D9199)
+
+
+val Beginner = Color(0xFF4CAF50)
+val Intermediate = Color(0xFFFFC107)
+val Advanced = Color(0xFFF44336)
