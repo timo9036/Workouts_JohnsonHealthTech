6ac1d5ac80b43577eb118918b10d8e998957427f
timo9036 <timo9036@hotmail.com>
2025-09-29 03:17:22 -0500

    added splashscreen

--- app/src/main/java/com/example/workouts_johnsonhealthtech/ui/navigation/Screen.kt
+++ app/src/main/java/com/example/workouts_johnsonhealthtech/ui/navigation/Screen.kt
@@ -1,6 +1,7 @@
 package com.example.workouts_johnsonhealthtech.ui.navigation
 
 sealed class Screen(val route: String) {
+    object Splash : Screen("splash")
     object WorkoutList : Screen("workoutList")
     object WorkoutDetail : Screen("workoutDetail/{workoutId}") {
         fun createRoute(workoutId: String) = "workoutDetail/$workoutId"
