262cf556b6cce7e0557c25cf8254418b72c046f3
timo9036 <timo9036@hotmail.com>
2025-09-28 21:56:18 -0500

    add dependency for testing

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,6 @@ plugins {
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
 
-    id("com.google.devtools.ksp") version "2.0.20-1.0.25" apply false
+    id("com.google.devtools.ksp") version "2.0.21-1.0.25" apply false
     id("com.google.dagger.hilt.android") version "2.57.1" apply false
 }
